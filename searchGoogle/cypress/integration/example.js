893660f357d3081064f6d8bef0cafa1e7777f172
Federico Dobal <fede.dobal@Federicos-MacBook-Pro.local>
2021-05-13 10:06:54 -0300

    Improve how to verify the first process automation date

--- searchGoogle/cypress/integration/example.js
+++ searchGoogle/cypress/integration/example.js
@@ -1,6 +1,6 @@
 describe('Search automation keyword on Google and ensure that the first automated process was implemented on 1785', () => {
 
-  it('loads search page', () => {
+  it('loads search page, search automation keyword, very conditions and take screenshot', () => {
     //GIVEN: the user visits google page and searches for automation keywork
     cy.visit('https://www.google.com');
 
@@ -13,7 +13,15 @@ describe('Search automation keyword on Google and ensure that the first automate
     // THEN: the year when the first industrial automated process was implemente was in 1785
     cy.get('.mw-parser-output > :nth-child(55)')
       .invoke("text")
-      .should("contains", "in 1785, making it the first completely automated");
+      .then((text) => {
+
+        // Not very explicit, verify by long string comparisson
+        expect(text).to.include('in 1785, making it the first completely automated')
+
+        // More explicit, only check the date, still string comparisson but only 4 characters long
+        const retrieved_date = text.split('Oliver Evans in')[1].split(',')[0].trim();
+        expect(retrieved_date).to.eq('1785')
+      });
 
     // take screenshot of the paragraph
     cy.get('.mw-parser-output > :nth-child(55)').screenshot('first_automated_process_paragraph')
