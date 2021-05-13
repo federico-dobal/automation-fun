2191c9646c00f759797605e0b9663e8f84ff4df9
Federico Dobal <fede.dobal@Federicos-MacBook-Pro.local>
2021-05-13 19:20:16 -0300

    Refactor cypress test case

---
+++ searchGoogle/README.md
@@ -0,0 +1,16 @@
+# Search _automation_ keyword on Google 
+
+## Description 
+ The goal of this project is to write an automated test case using Cypress to Search _automation_ keyword on Google and ensure that the first automated process was implemented on 1785.
+ 
+ ## How to start Cypress test execution
+Execute the following command to start the Cypress test execution:
+ 
+         npx cypress open
+         
+Then, choose the *search_google.js* file:
+
+![Alt text](resources/cypress-env.png?raw=true )     
+
+Click on it and the test execution will start.    
+          
--- searchGoogle/cypress/integration/search_google.js
+++ searchGoogle/cypress/integration/search_google.js
@@ -1,16 +1,15 @@
 describe('Search automation keyword on Google and ensure that the first automated process was implemented on 1785', () => {
 
   it('loads search page, search automation keyword, very conditions and take screenshot', () => {
-    //GIVEN: the user visits google page and searches for automation keywork
+    //GIVEN: the user visits Google page and searches for automation keywork
     cy.visit('https://www.google.com');
 
     cy.get('input[name="q"]').type('automation{enter}');
 
     // WHEN: the user clicks on Wikipedia link
-    cy.get('[href="https://en.wikipedia.org/wiki/Automation"]> .LC20lb')
-    .click();
+    cy.get('[href="https://en.wikipedia.org/wiki/Automation"]> .LC20lb').click();
 
-    // THEN: the year when the first industrial automated process was implemente was in 1785
+    // THEN: verify the year when the first industrial automated process was implemented
     cy.get('.mw-parser-output > :nth-child(55)')
       .invoke("text")
       .then((text) => {
@@ -18,12 +17,12 @@ describe('Search automation keyword on Google and ensure that the first automate
         // Not very explicit, verify by long string comparisson
         expect(text).to.include('in 1785, making it the first completely automated')
 
-        // More explicit, only check the date, still string comparisson but only 4 characters long
+        // More explicit, only check the date, still string comparison but only 4 characters long
         const retrieved_date = text.split('Oliver Evans in')[1].split(',')[0].trim();
         expect(retrieved_date).to.eq('1785')
       });
 
-    // take screenshot of the paragraph
+    // take paragraph screenshot
     cy.get('.mw-parser-output > :nth-child(55)').screenshot('first_automated_process_paragraph')
   });
 
